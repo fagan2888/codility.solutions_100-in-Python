2778c63900d673731170b2a5b9a4568235a03411
j-cunanan <jsonm11@gmail.com>
2019-11-02 09:55:32 +0900

    Create OddOccurrencesInArray.py

---
+++ OddOccurrencesInArray.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  2 00:39:19 2019
+
+@author: jcunanan
+"""
+
+
+
+def solution(A):
+    
+    if len(A) //2 == 1:
+       raise ValueError('String needs to be odd length array')
+    
+    no_pair = dict()
+    
+    for num in A:
+       if num in no_pair:
+          del no_pair[num]
+       else:
+           no_pair[num]=num
+    
+    for i in no_pair:
+        odd = no_pair[i]
+    
+    return odd
+
